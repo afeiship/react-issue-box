29222c01ebea7ff5a9aef4e781321223a8dba0b7
afeiship <1290657123@qq.com>
2024-06-22 08:54:18 +0800

    feat: add hoverable + shadowAble

--- packages/lib/src/index.tsx
+++ packages/lib/src/index.tsx
@@ -20,6 +20,11 @@ export type ReactIssueBoxProps = {
    */
   hoverAble?: boolean;
   /**
+   * The shadow effect for component.
+   * @default false
+   */
+  shadowAble?: boolean;
+  /**
    * The children element.
    */
   children?: ReactNode;
@@ -29,13 +34,15 @@ export default class ReactIssueBox extends Component<ReactIssueBoxProps> {
   static displayName = CLASS_NAME;
   static version = '__VERSION__';
   static defaultProps = {
-    hoverAble: false
+    hoverAble: false,
+    shadowAble: false
   };
 
   render() {
-    const { className, contentClassName, children, hoverAble, ...rest } = this.props;
+    const { className, contentClassName, children, hoverAble, shadowAble, ...rest } = this.props;
     return (
-      <section data-component={CLASS_NAME} data-hoverable={hoverAble} className={cx(CLASS_NAME, className)} {...rest}>
+      <section data-component={CLASS_NAME} data-hoverable={hoverAble} data-shadowable={shadowAble}
+               className={cx(CLASS_NAME, className)} {...rest}>
         <div className={cx(`${CLASS_NAME}__inner`)}>
           <div className={cx(`${CLASS_NAME}__content`, contentClassName)}>
             {children}
