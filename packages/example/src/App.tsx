1871b8d6880b8885e3086185854f75fc2f8c4d96
afeiship <1290657123@qq.com>
2024-06-22 09:03:06 +0800

    fix: add content default padding

--- packages/example/src/App.tsx
+++ packages/example/src/App.tsx
@@ -9,10 +9,10 @@ function App() {
       </div>
       <h1>react-issue-box</h1>
       <div className="y-5">
-        <ReactIssueBox className="wp-5 mx-auto" contentClassName="p-5">
+        <ReactIssueBox className="wp-5 mx-auto">
           清晨醒来，打开窗帘，一抹慵懒的阳光照进来，暖暖的，柔柔的，时光瞬间变得温婉静美，打开音乐，沏一杯花茶，躺在床上，暖阳淼淼，茶香淡淡，音乐袅袅，闭上眼睛，嘴角轻轻上扬，算是对着光阴的镜子，和自己撒个娇。
         </ReactIssueBox>
-        <ReactIssueBox hoverAble shadowAble className="wp-5 mx-auto" contentClassName="p-5">
+        <ReactIssueBox hoverAble shadowAble className="wp-5 mx-auto">
           <h6 className="mb-2 font-bold">需要讨论，原因如下</h6>
           <ol className="ml-5 list-decimal text-sm">
             <li>我的选校，对应原来的建议书，但建议书需要 report_id/或者 report_id + batch_id; my-schools
